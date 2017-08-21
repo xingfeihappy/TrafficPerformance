ddfae0b5603fd087e1d2f1823f3bf1919f81ee21
zwp <1258271806@qq.com>
2017-08-21 12:54:20 +0800

    初步完成了帮助文档

--- src/views/systemSetting/helpDoc.vue
+++ src/views/systemSetting/helpDoc.vue
@@ -1,14 +1,8 @@
 <template>
     <section>
-
-       <div>
-           <span>
-               帮助文档
-           </span>
-       </div>
-        <el-row >
-            <el-col ></el-col>
-            <el-col >
+        <el-row :span="24">
+            <el-col :span="8"></el-col>
+            <el-col :span="8">
                     <!-- <el-upload
                     :action="murl"
                     :data = "data"
@@ -22,19 +16,47 @@
                     <input type="file"  @change="upload">  
             </el-col>
         </el-row>
+
+        <el-row >
+            <el-col :span="20">
+                <div>
+                    <el-button id="prev" @click="onPrevPage">Previous</el-button>
+                    <el-button id="next" @click="onNextPage">Next</el-button>
+                    &nbsp; &nbsp;
+                    <span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
+                </div>
+            </el-col>
+        </el-row>
+
+        <el-row>
+                <canvas id="the-canvas" class = "pdf"></canvas>
+        </el-row>
+
+
+
     </section> 
 </template>
 
 <script>
+    PDFJS.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
 export default {
     data() {
         return {
+            pageNum:1,
+            pageRendering:false,
+            pdfDoc:null,
+            pageNumPending:null,
+            scale : 0.8,
+            canvas : {},
+            ctx:{},
             murl:this.Constant.ajaxAddress+this.Constant.upload,
+            durl:this.Constant.ajaxAddress+this.Constant.download,
             data:{},
             headers:{'Access-Control-Allow-Origin':'*','Content-Type':'multipart/form-data'}
         }
     },
+
     methods:{
         upload(event){
             console.log(event);
@@ -46,29 +68,105 @@ export default {
             var fd = new FormData();
             fd.append('file',pdfFile);
             fd.append('token', this.$token);
-
+            var _this = this;
             this.$http.post(this.murl,fd,{emulateJSON: true}).then(res =>{
-                console.log(res)
-            },err =>{ console.log('err')})
-
-
+                _this.$message('上传成功');
+                 _this.loadPdf();
+                console.log(res);
+            },err =>{ 
+                _this.$message('上传失败');console.log('err');})
         },
+        handleRemove(file, fileList) {
+            console.log(file, fileList);
+        },
+        handlePreview(file) {
+            console.log(file);
+        },
+        beforeUpload(file){
+            console.log('before upload',this.headers);
+            
+            this.data.token = this.$token;   
+        },
+        loadPdf(){
+            this.pageNum=1;
+            this.pageRendering=false;
+            this.pdfDoc=null;
+            this.pageNumPending=null;
+            var _this = this;
+            PDFJS.getDocument(this.durl).then(function(pdfDoc_) {
+                console.log('pdfDoc',pdfDoc_);
+                _this.pdfDoc = pdfDoc_;
+                console.log('pdfDoc',_this.pdfDoc);
+                document.getElementById('page_count').textContent = _this.pdfDoc.numPages;
+                // Initial/first page rendering
+                _this.renderPage(_this.pageNum);
+            });
+        },
+        renderPage(num) {
+            var _this = this;
+            this.pageRendering = true;
+            // Using promise to fetch the page
+            this.pdfDoc.getPage(num).then(function(page) {
+                var viewport = page.getViewport(_this.scale);
+                _this.canvas.height = viewport.height;
+                _this.canvas.width = viewport.width;
 
+                // Render PDF page into canvas context
+                var renderContext = {
+                canvasContext: _this.ctx,
+                viewport: viewport
+                };
+                var renderTask = page.render(renderContext);
 
-      handleRemove(file, fileList) {
-        console.log(file, fileList);
-      },
-      handlePreview(file) {
-        console.log(file);
-      },
-      beforeUpload(file){
-           console.log('before upload',this.headers);
-           
-          this.data.token = this.$token;   
-      }
-
-
+                // Wait for rendering to finish
+                renderTask.promise.then(function() {
+                _this.pageRendering = false;
+                if (_this.pageNumPending !== null) {
+                    // New page rendering is pending
+                    renderPage(_this.pageNumPending);
+                    _this.pageNumPending = null;
+                }
+                });
+            });
+            // Update page counters
+            document.getElementById('page_num').textContent = this.pageNum;
+        },
+        queueRenderPage(num) {
+            if (this.pageRendering) {
+                this.pageNumPending = num;
+            } else {
+                this.renderPage(num);
+            }
+        },
+        onPrevPage() {
+            if (this.pageNum <= 1) {
+                return;
+            }
+            this.pageNum--;
+            this.queueRenderPage(this.pageNum);
+        },
+        onNextPage() {
+            if (this.pageNum >= this.pdfDoc.numPages) {
+                return;
+            }
+            this.pageNum++;
+            this.queueRenderPage(this.pageNum);
+        }
+    },
+    mounted:function(){
+        this.canvas = document.getElementById('the-canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.loadPdf();
     }
+
  }
 </script>
+
+<style scoped lang="scss">
+    .pdf{
+        border:1px solid black;
+    }
+
+</style>
+
 
