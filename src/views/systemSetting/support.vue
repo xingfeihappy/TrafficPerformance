cd3aef3f8acf6eea20a29183e2344bd3185077dd
cn-xia <330754175@qq.com>
2017-08-24 15:32:07 +0800

    技术支持 帮助文档修改

--- src/views/systemSetting/support.vue
+++ src/views/systemSetting/support.vue
@@ -1,10 +1,25 @@
 <template>
     <section>
-       <div>
-           <span>
-               技术支持
-           </span>
-       </div>
+        <el-row>
+            <el-col :span= 8><div class="grid-content"></div></el-col>
+            <el-col :span= 8>               
+                <el-card>
+                    <div slot="header" >
+                        <span style="line-height: 15px;">技术支持</span>
+                    </div>
+                    <div class="item">
+                        {{'名称：浙江电子口岸有限公司' }}
+                    </div>
+                    <div class="item">
+                        {{'电话：967209' }}
+                    </div>
+                    <div class="item">
+                        {{'邮箱：cs@zjport.gov.cn' }}
+                    </div>
+                </el-card>       
+            </el-col>
+            <el-col :span= 8><div class="grid-content"></div></el-col>
+        </el-row>
     </section> 
 </template>
 
@@ -18,3 +33,14 @@ export default {
  }
 </script>
 
+<style scoped lang="scss">
+    .grid-content{
+         min-height: 36px;
+    }
+    .el-row{
+        margin-top:100px;
+    }
+    .item{
+        padding: 15px 5px;
+    }
+</style>
